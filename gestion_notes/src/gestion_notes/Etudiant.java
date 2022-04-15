e58a733b39d691abe77d8e1608bdb1b7c32ab7a5
Bilal <Bilal@192.168.56.1>
2022-04-15 14:40:06 +0000

    redefinition de la methode toString

--- gestion_notes/src/gestion_notes/Etudiant.java
+++ gestion_notes/src/gestion_notes/Etudiant.java
@@ -7,6 +7,10 @@ public class Etudiant {
 	public Etudiant(int id, String nom ) {
 		this.id=id; this.nom=nom; this.note=10;
 	}
+	@Override
+	public String toString() {
+		return "("+this.nom+":"+this.note+")";
+	}
 	
 	
 	
