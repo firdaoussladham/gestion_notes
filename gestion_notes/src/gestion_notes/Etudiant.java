11fb08916e047f23ca2e33faf0cf682b7c070a49
Bilal <Bilal@192.168.56.1>
2022-04-15 14:33:08 +0000

    declaration des variables

--- gestion_notes/src/gestion_notes/Etudiant.java
+++ gestion_notes/src/gestion_notes/Etudiant.java
@@ -1,22 +1,9 @@
 package gestion_notes;
 
 public class Etudiant {
-	int id;
-	String nom;
-	double note;
-	public Etudiant() {
-		// TODO Auto-generated constructor stub
-	}
-	public Etudiant(int id, String nom) {
-		this.id=id; this.nom=nom; this.note=10;
-	}
-	@Override
-	public String toString() {
-		return (this.nom+":"+this.note);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		
-		return super.equals(obj);
-	}
+	private int id;
+	private String nom;
+	private double note;
+	
+	
 }
