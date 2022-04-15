c2740b40e2b126c4b4ceb30025f5d03ead09a946
Bilal <Bilal@192.168.56.1>
2022-04-15 14:39:15 +0000

    creation d'un constructeur

--- gestion_notes/src/gestion_notes/Etudiant.java
+++ gestion_notes/src/gestion_notes/Etudiant.java
@@ -4,6 +4,11 @@ public class Etudiant {
 	private int id;
 	private String nom;
 	private double note;
+	public Etudiant(int id, String nom ) {
+		this.id=id; this.nom=nom; this.note=10;
+	}
+	
+	
 	
 	
 }
